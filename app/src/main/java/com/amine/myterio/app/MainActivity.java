550a3e8fcef197f69715d1917e5e4bb70701ad93
aminecmi <aminecmi@gmail.com>
2015-07-12 17:28:29 +0200

    frag 1

--- app/src/main/java/com/amine/myterio/app/MainActivity.java
+++ app/src/main/java/com/amine/myterio/app/MainActivity.java
@@ -1,24 +1,9 @@
 package com.amine.myterio.app;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import com.amine.myterio.app.adapters.CitiesAdapter;
-import com.amine.myterio.app.config.Config;
-import com.amine.myterio.app.db.CityDAO;
-import com.amine.myterio.app.model.City;
-import com.melnykov.fab.FloatingActionButton;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,61 +11,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        handleView();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        handleView();
-    }
-
-    private void saveUserCountry() {
-        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        Config.country = tm.getSimCountryIso().toLowerCase();
-        if (Config.country == null || Config.country.equals("") || Config.country.isEmpty())
-            Config.country = MainActivity.this.getResources().getConfiguration().locale.getCountry().toLowerCase();
-    }
-
-    private void handleView() {
         setContentView(R.layout.activity_main);
-        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.list);
-        saveUserCountry();
-
-        // Google way to test is tablet
-        boolean isTablet = ((this.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE);
-
-        final RecyclerView.LayoutManager layoutManager;
-        if (isTablet) {
-            layoutManager = new GridLayoutManager(this, 2);
-        }
-        else {
-            layoutManager = new LinearLayoutManager(this);
-        }
-
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.setHasFixedSize(true);
-
-        ArrayList<City> cities;
-
-        CityDAO dao = CityDAO.getInstance(this);
-        cities = dao.getAllCities();
-
-        RecyclerView.Adapter mAdapter = new CitiesAdapter(cities, this);
-        mRecyclerView.setAdapter(mAdapter);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.attachToRecyclerView(mRecyclerView);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                startActivity(intent);
-            }
-        });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/amine/myterio/app/fragments/CitiesListFragment.java
@@ -0,0 +1,95 @@
+package com.amine.myterio.app.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.telephony.TelephonyManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.amine.myterio.app.R;
+import com.amine.myterio.app.SearchActivity;
+import com.amine.myterio.app.adapters.CitiesAdapter;
+import com.amine.myterio.app.config.Config;
+import com.amine.myterio.app.db.CityDAO;
+import com.amine.myterio.app.model.City;
+import com.melnykov.fab.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class CitiesListFragment extends Fragment {
+
+    RecyclerView mRecyclerView;
+    FloatingActionButton fab;
+    private Activity mActivity;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mActivity = activity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View fragView = inflater.inflate(R.layout.cities_list_fragment, null, false);
+        mRecyclerView = (RecyclerView) fragView.findViewById(R.id.list);
+        fab = (FloatingActionButton) fragView.findViewById(R.id.fab);
+        handleView();
+        return fragView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        handleView();
+    }
+
+    private void handleView() {
+        saveUserCountry();
+
+        // Google way to test is tablet
+        boolean isTablet = ((this.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE);
+
+        RecyclerView.LayoutManager layoutManager;
+        if (isTablet) {
+            layoutManager = new GridLayoutManager(mActivity, 2);
+        } else {
+            layoutManager = new LinearLayoutManager(mActivity);
+        }
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        ArrayList<City> cities;
+
+        CityDAO dao = CityDAO.getInstance(mActivity);
+        cities = dao.getAllCities();
+
+        RecyclerView.Adapter mAdapter = new CitiesAdapter(cities, mActivity);
+        mRecyclerView.setAdapter(mAdapter);
+
+        fab.attachToRecyclerView(mRecyclerView);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mActivity, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private void saveUserCountry() {
+        TelephonyManager tm = (TelephonyManager) mActivity.getSystemService(Context.TELEPHONY_SERVICE);
+        Config.country = tm.getSimCountryIso().toLowerCase();
+        if (Config.country == null || Config.country.equals("") || Config.country.isEmpty())
+            Config.country = mActivity.getResources().getConfiguration().locale.getCountry().toLowerCase();
+    }
+}
